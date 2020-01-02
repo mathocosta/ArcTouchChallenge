9d4141935694e0531969ac8e69a1f8624ea345e6
Matheus Oliveira Costa <matheusoliveiracos@gmail.com>
2020-01-02 13:01:37 -0300

    Project configuration

---
+++ ArcTouchChallege/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  ArcTouchChallege
+//
+//  Created by Matheus Oliveira Costa on 02/01/20.
+//  Copyright © 2020 mathocosta. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        if #available(iOS 13.0, *) {
+            return true
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+
+            let rootViewController = QuestionViewController()
+            window?.rootViewController = rootViewController
+
+            window?.makeKeyAndVisible()
+
+            return true
+        }
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+}
